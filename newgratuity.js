51582af4bfcddcf6939cd0953a6e863d59b94acd
zezokam <125836314+zezokam@users.noreply.github.com>
2023-09-01 19:23:49 +0400

    Update newgratuity.js

--- newgratuity.js
+++ newgratuity.js
@@ -23,56 +23,31 @@ function calculateGratuity() {
   // Calculate daily wage
   var dailyWage = lastBasicWage / 30;
 
-  // Calculate gratuity amount
-  var gratuityAmount = 0;
-  if (yearsOfService < 3) {
-    gratuityAmount = (dailyWage * 15 * yearsOfService) + (dailyWage * monthsOfService * 15 / 12) + (dailyWage * daysOfService / 12);
-  } else {
-    gratuityAmount = (dailyWage * 15 * 3) + (dailyWage * 30 * (yearsOfService - 3)) + (dailyWage * monthsOfService * 30 / 12) + (dailyWage * daysOfService / 12);
-  }
+  // Calculate gratuity amount (consistent 30 days per year)
+  var gratuityAmount = dailyWage * 30 * yearsOfService + dailyWage * 30 * (monthsOfService / 12) + dailyWage * (daysOfService / 12);
 
   // Format duration of service
   var yearsText, monthsText, daysText;
   if (yearsOfService == 1) {
     yearsText = "سنة";
-  } else if (yearsOfService == 2) {
-    yearsText = "سنتين";
-  } else if (yearsOfService >= 3 && yearsOfService <= 10) {
-    yearsText = "سنوات";
   } else {
-    yearsText = "سنة";
+    yearsText = "سنوات";
   }
 
   if (monthsOfService == 1) {
     monthsText = "شهر";
-  } else if (monthsOfService == 2) {
-    monthsText = "شهرين";
-  } else if (monthsOfService >= 3 && monthsOfService <= 10) {
-    monthsText = "أشهر";
   } else {
-    monthsText = "شهر";
+    monthsText = "أشهر";
   }
 
   if (daysOfService == 1) {
     daysText = "يوم";
-  } else if (daysOfService == 2) {
-    daysText = "يومين";
-  } else if (daysOfService >= 3 && daysOfService <= 10) {
-    daysText = "أيام";
   } else {
-    daysText = "يوم";
+    daysText = "أيام";
   }
 
   // Display result
   var duration = yearsOfService + " " + yearsText + " و " + monthsOfService + " " + monthsText + " و " + daysOfService + " " + daysText;
   var resultString = "قيمة المكافأة: " + gratuityAmount.toFixed(3) + " ريال" + "<br>" + "مدة الخدمة: " + duration;
   document.getElementById("result").innerHTML = resultString;
-}
-
-function changeLanguage() {
-  // Toggle language
-  var languageButton = document.getElementById("languageButton");
-  
-  // Redirect to index.html page
-  window.location.href = "en.html";
 }
