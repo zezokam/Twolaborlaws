4b7a5810d781705ef01bf9ff8d7625433a542f87
zezokam <125836314+zezokam@users.noreply.github.com>
2023-09-01 23:15:56 +0400

    Update gratuity.js

--- gratuity.js
+++ gratuity.js
@@ -65,7 +65,7 @@ function calculateGratuity() {
 
   // Display result
   var duration = yearsOfService + " " + yearsText + " و " + monthsOfService + " " + monthsText + " و " + daysOfService + " " + daysText;
-  var resultString = "قيمة المكافأة: " + gratuityAmount.toFixed(3) + " ريال" + "<br>" + "مدة الخدمة: " + duration;
+  var resultString = "مبلغ المكافأة: " + gratuityAmount.toFixed(3) + " ريال" + "<br>" + "مدة الخدمة: " + duration;
   document.getElementById("result").innerHTML = resultString;
 }
 
